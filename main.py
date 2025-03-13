c0f94fb1058e42f9ef5dd8d8ee50342b7b1c39bc
matthew-jenkins3 <matthewjenkins@dal.ca>
2025-03-13 15:54:09 -0400

    New Data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from xyz_positioner import Motor
 from TekronixMDO4000 import take_mesurment, exp_to_csv
 from SerialArduino import SerialArduino
 from time import sleep
+from datetime import datetime
 
 import csv
 
@@ -48,15 +49,17 @@ def protocol_002():
 
     name = '20v_FP133-v2'
 
-    x_samples = 1 #number of samples in x
+    x_samples = 5 #number of samples in x
     y_samples = 200 # number of samples in y
 
-    x_steps = 100   # number of steps between samples in x
+    x_steps = 500   # number of steps between samples in x
     y_steps = 100   # number of steps between samples in x
 
     repeat_num = 5 # number of times the measurment will be re-taken
 
-    inital_position = (6500, 0, 20100) #starting position of the hydrophone in steps (x,y,z)
+    inital_position = (6000, 0, 20100) #starting position of the hydrophone in steps (x,y,z)
+
+    now = datetime.now()
 
     motor = Motor(com_port='COM8', baudrate=9600, step_size=1, speed=1)
     hifu_pulse = SerialArduino(com_port='COM7', baudrate=9600, timeout=1)
@@ -64,6 +67,7 @@ def protocol_002():
     data_max = np.zeros((x_samples, y_samples, repeat_num)) # a 3d array holding the maximum pressure recorded at each location
     data_min = np.zeros((x_samples, y_samples, repeat_num)) # a 3d array holding the minmum pressure recorded at each location
     data_rms = np.zeros((x_samples, y_samples, repeat_num)) # a 3d array holding the minmum pressure recorded at each location
+    data_raw = np.zeros((x_samples, y_samples, repeat_num, 10000)) # a 4d array holding the raw data (just saving the osciliscope waveform in the 4th dimention)
 
     '''
     data_max and data_min are orginized into a 3d array
@@ -95,12 +99,14 @@ def protocol_002():
                 data_max[x][y][num] = max(measurement)
                 data_min[x][y][num] = min(measurement)
                 data_rms[x][y][num] = np.sqrt(np.mean(np.square(measurement)))
+                data_raw[x][y][num][:] = measurement
                 print(f'location: ({x},{y}) measurement number:{num} | max [V]: {max(measurement)} | min [V]: {min(measurement)} | RMS [v]: {np.sqrt(np.mean(np.square(measurement)))}')
         motor.move(0, -(y_samples*y_steps), 0) # return to the start of the scan line
 
-    np.save(f'data_max_{name}.npy', data_max)
-    np.save(f'data_min_{name}.npy', data_min)
-    np.save(f'data_rms_{name}.npy', data_rms)
+    np.save(f'{now.strftime("%d%m%Y")}/data_max_{name}.npy', data_max)
+    np.save(f'{now.strftime("%d%m%Y")}/data_min_{name}.npy', data_min)
+    np.save(f'{now.strftime("%d%m%Y")}/data_rms_{name}.npy', data_rms)
+    np.save(f'{now.strftime("%d%m%Y")}/data_raw_{name}.npy', data_raw)
 
 
 if __name__ == '__main__':
